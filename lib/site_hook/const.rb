8172a55b7c199ae71af3796d122f9b61ba5f8084
Ken Spencer <me@iotaspencer.me>
2019-02-06 12:21:21 -0500

    update SHRC constant to config class

--- lib/site_hook/const.rb
+++ lib/site_hook/const.rb
@@ -1,10 +1,11 @@
 require 'site_hook/log'
 require 'site_hook/logger'
+require 'site_hook/config'
 module SiteHook
   class Consts
     HOOKLOG = SiteHook::HookLogger::HookLog.new(SiteHook::Logs.log_levels['hook']).log
     BUILDLOG = SiteHook::HookLogger::BuildLog.new(SiteHook::Logs.log_levels['build']).log
     APPLOG = SiteHook::HookLogger::AppLog.new(SiteHook::Logs.log_levels['app']).log
-    SHRC = YAML.load_file(Pathname(Dir.home).join('.jph', 'config'))
+    SHRC = SiteHook::Config.new.config.to_h
   end
 end
